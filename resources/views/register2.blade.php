df0fca669d09002d6fdbbeb2973c11ec04ae13d6
hamasasi12 <hamasakifsanie@gmail.com>
2025-06-07 19:41:18 +0800

    merging

--- resources/views/register2.blade.php
+++ resources/views/register2.blade.php
@@ -248,7 +248,7 @@
                 <!-- Village -->
                 <div>
                     <label for="village" class="block mb-2 text-sm font-medium text-gray-700">
-                        Village
+                        Villagej
                     </label>
                     <div class="relative">
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
