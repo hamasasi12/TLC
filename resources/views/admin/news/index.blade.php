7d6ebd7ef39fd52d412a28b78154fd59f18ce501
hamasasi12 <hamasakifsanie@gmail.com>
2025-06-28 23:50:51 +0800

    commit

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -263,7 +263,6 @@
         </div>
     </div>
 
-
     <!-- Popovers -->
     <div data-popover id="popover-delete" role="tooltip"
         class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0">
@@ -297,5 +296,4 @@
         </div>
         <div data-popper-arrow></div>
     </div>
-
 @endsection
